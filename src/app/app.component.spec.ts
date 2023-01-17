6c1c10f462547f42c8dd04d480eca47227389ef1
ringgo5 <vicentem424@gmail.com>
2023-01-17 13:38:22 +0100

    1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let app: AppComponent;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    app = new AppComponent();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+ 
 
-  it(`should have as title 'prueba'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('prueba');
-  });
+ it('la suma debe dar 4' async(()=>{
+
+  expect(app.add(2,2)).toEqual(4);
+
+
+ }))
+
   
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('prueba app is running!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prueba';
+export class AppComponent implements OnInit {
+  @ViewChild('result') result: any;
+  title = 'ng-jasmine';
+  public ngOnInit(): void {
+    
+  }
+  public add(num1: number, num2 : number): number{
+    return Number(num1) + Number(num2);
+  }
+  public printAdd(num1:number, num2:number): any{
+    this.result.nativeElement.value = this.add(num1,num2);
+  }
 }
