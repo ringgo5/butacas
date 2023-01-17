2aeb5cb2792045560564d9a3ddf7cc81245c4c9c
ringgo5 <vicentem424@gmail.com>
2023-01-17 13:38:16 +0100

    1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,7 +25,7 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('prueba');
   });
-
+  
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
